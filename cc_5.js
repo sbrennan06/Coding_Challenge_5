924d936fe0b66e5b05c2854e1941e65a59195d83
Samantha Brennan <samanthabrennan@usf.edu>
2025-09-27 21:50:54 -0400

    added function to calculate base pay

--- cc_5.js
+++ cc_5.js
@@ -12,4 +12,21 @@ const employees = [
 employees.forEach(employee => console.log(`Employee Name: ${employee.name}, Hourly Rate: ${employee.hourlyRate}, Hours Worked: ${employee.hoursWorked}`));
 
 
-//step 3: 
+//Step 3: Write function to calculate base pay
+
+function calculateBasePay(hourlyRate, hoursWorked) {
+    const baseHours = Math.min(hoursWorked, 40);
+    return baseHours * hourlyRate; // hourly rate on hours up to and including 40
+}
+
+//Step 4: Write a function to calculate overtime pay
+function calculateOvertimePay(hourlyRate, hoursWorked) {
+    const overtimeHours = Math.max(hoursWorked - 40, 0);
+    return overtimeHours * hourlyRate * 1.5; // time and a half on hours over 40
+    }
+
+employees.forEach(({name, hourlyRate, hoursWorked}) => 
+
+)
+
+console.log()
