1ef815f6b66aa5d65ed773628e5e111a4a57eb63
Samantha Brennan <samanthabrennan@usf.edu>
2025-09-27 21:33:17 -0400

    Added array of employee objects & console log to test code

---
+++ cc_5.js
@@ -0,0 +1,15 @@
+//Coding challenge 5
+
+//Step 2: create array of 3-5 emmployee objects
+
+const employees = [
+    {name: "Rusty Spoons", hourlyRate: 48.39, hoursWorked: 27},
+    {name: "Hubert Cumberdale", hourlyRate: 32.74, hoursWorked: 41},
+    {name: "Marjory Stewart-Baxter", hourlyRate: 24.13, hoursWorked: 49},
+    {name: "Jeremy Fisher", hourlyRate: 3.75, hoursWorked: 101},
+];
+
+employees.forEach(employee => console.log(`Employee Name: ${employee.name}, Hourly Rate: ${employee.hourlyRate}, Hours Worked: ${employee.hoursWorked}`));
+
+
+//step 3: 
